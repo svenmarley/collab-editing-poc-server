ad9c38ad80f126a0d7e1810d78ee4b249305a811
Mike Anderson <mikea@candicontrols.com>
2021-06-10 10:55:29 -0700

    feat(Mutation and MutationPeer classes): Mutation and MutationPeer classes
    Created these to interface to the tblMUTATIONS mySQL table.
    
    Decided
    to mvoe from a file based system to a db based system.  Originally thot
    the files would be easier, but turned out .... not

---
+++ src/models/Mutation.js
@@ -0,0 +1,95 @@
+// saf
+const { UserPeer } = require( './UserPeer' );
+const { MutationPeer } = require( './MutationPeer' );
+
+class Mutation {
+    #sFunc = 'Mutation';
+    ID = null;
+    conversationId = null;
+    userId = null;
+    origin = null;    // todo: all should be made private
+    mutation = null;
+    auditDateTime = null;
+    className = 'Mutation';
+
+    constructor( inConversationId, inAuthor, inOrigin, inMutation ) {
+        const sFunc = this.#sFunc + '.constructor()-->';
+        const debug = true;
+
+        this.conversationId = inConversationId;
+        this.origin = inOrigin;
+        this.mutation = inMutation;
+
+        if ( typeof ( inAuthor ) === 'string' ) {
+            let user = UserPeer.getUser( inAuthor );
+            debug && console.log( sFunc + 'inAuthor', inAuthor, 'user', user );
+            this.userId = user.id;
+        }
+        else {
+            this.userId = inAuthor;
+        }
+
+    }
+
+    toString() {
+        return `ID[${this.ID}] conversationId[${this.conversationId}] origin[${this.origin}] mutation[${this.mutation}]`;
+    }
+
+    save() {
+        let sFunc = this.#sFunc + '.save( )-->';
+        const debug = true;
+
+        debug && console.log( sFunc + 'this.mutation', this.toString() );
+
+        return new Promise( ( respond, reject ) => {
+
+            let sQuery = '';
+            let vars = [];
+            MutationPeer.findOne( this )
+                        .then( ( mut ) => {
+                            sFunc += '.MutationPeer.findOne().then()-->';
+                            debug && console.log( sFunc + 'returned mut', mut );
+
+                            if ( mut ) {
+                                // updating old rec
+                                vars = [ this.conversationId, this.origin, this.mutation, this.ID ];
+                                sQuery = 'UPDATE tblMUTATIONS set CONVERSATION_ID = ?, ORIGIN = ?, MUTATION = ? where ID = ? ';
+                                let go = dbConnection.query( sQuery, vars, ( err, rows ) => {
+                                    debug && console.log( sFunc + 'err', err, 'rows', rows );
+                                } );
+                                console.log( sFunc + 'sql', go.sql );
+
+                                respond( true );
+                            }
+                            else {
+                                // inserting new rec
+                                sQuery = 'INSERT INTO tblMUTATIONS SET ?';
+                                vars = {
+                                    CONVERSATION_ID : this.conversationId,
+                                    ORIGIN : this.origin,
+                                    MUTATION : this.mutation,
+                                    USER_ID : this.userId,
+                                };
+                                let go = dbConnection.query( sQuery, vars, ( err, results ) => {
+                                    debug && console.log( sFunc + 'err', err, 'results', results );
+
+                                    if ( results.affectedRows === 1 ) {
+                                        this.ID = results.insertId;
+                                        respond( { ID : this.ID } );     // todo: should respond with 204 for update and 200 for insert
+                                    }
+                                    console.log( sFunc + 'sql', go.sql );
+                                } );
+                            }
+
+                        } )
+                        .catch( ( e ) => {
+                            console.log( sFunc + 'errX', e );
+                            reject( 'Record not updated' );
+
+                        } );
+        } );
+
+    }
+}
+
+module.exports = { Mutation };
---
+++ src/models/MutationPeer.js
@@ -0,0 +1,92 @@
+class MutationPeer {
+
+    static findOne( searchStruct = {} ) {
+        const sFunc = 'MutationPeer.findOne()-->';
+        const debug = false;
+
+        return new Promise( ( respond, reject ) => {
+            MutationPeer.find( searchStruct )
+                        .then( ( aMutations ) => {
+                            debug && console.log( sFunc + 'isArray(aMutations)', Array.isArray( aMutations ), 'aMutations', aMutations );
+                            respond( aMutations.length ? aMutations[0] : null );
+                        } )
+                        .catch( ( e ) => {
+                            console.log( sFunc + 'e', e );
+                            reject( e );
+                        } );
+
+        } );
+    }
+
+    static find( searchStruct = {} ) {
+        const { Mutation } = require( './Mutation' );
+
+        const sFunc = 'MutationPeer::find()-->';
+        const debug = true;
+
+        debug && console.log( sFunc + 'searchStruct', searchStruct );
+        let query;
+        let vars = [];
+        if ( typeof ( searchStruct ) === 'string' ) {
+            query = 'SELECT * FROM tblMUTATIONS where ORIGIN like ?';
+            vars.push( searchStruct.replace( '*', '%' ) );
+        }
+        else if ( typeof ( searchStruct ) === 'object' ) {
+            if ( typeof ( searchStruct.className ) === 'string' ) {
+                const mut = searchStruct;
+
+                query = 'SELECT * FROM tblMUTATIONS where ';
+                query += ' CONVERSATION_ID = ?';
+                vars.push( mut.conversationId );
+                query += ' and ORIGIN = ? ';
+                vars.push( mut.origin );
+            }
+            else {
+                //{ ORIGIN : inOrigin }
+                let fields = [];
+                let tVars = [];
+                Object.keys( searchStruct ).map( ( f ) => {
+                    fields.push( f );
+                    tVars.push( searchStruct[f] );
+                } );
+                console.log( sFunc + 'fields', fields, 'tVars', tVars );
+
+                query = 'SELECT * FROM tblMUTATIONS where ';
+                for ( let x = 0; x < fields.length; x++ ) {
+                    query += ` ${fields[x]} = ?`;
+                    vars.push( tVars[x] );
+                }
+            }
+        }
+        debug && console.log( sFunc + 'query', query, 'vars', vars );
+
+        return new Promise( ( respond, /*reject*/ ) => {
+            let go = dbConnection.query( query, vars, ( err, rows ) => {
+                debug && console.log( sFunc + 'rows ', rows );
+                if ( !err ) {
+                    let aMutations = [];
+                    rows.map( ( row ) => {
+                        let mut = new Mutation();
+                        mut.ID = row.ID;
+                        mut.conversationId = row.CONVERSATION_ID;
+                        mut.origin = row.ORIGIN;
+                        mut.mutation = row.MUTATION;
+                        mut.auditDateTime = row.AUDIT_DATE_TIME;
+                        mut.userId = row.USER_ID;
+
+                        console.log( sFunc + 'mut', mut );
+
+                        aMutations.push( mut );
+
+                    } );
+                    console.log( sFunc + 'returning', aMutations );
+                    respond( aMutations );
+                }
+            } );
+            console.log( sFunc + 'sql', go.sql );
+
+        } );
+    }
+}
+
+module.exports = { MutationPeer };
