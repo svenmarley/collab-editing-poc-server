f7a205abd0851801f5a1bf757a440285036a51b2
Mike Anderson <mikea@candicontrols.com>
2021-06-13 16:07:00 -0700

    feat(shared):
    * added breakMutation for returning an object with the parts of a mutation  (alison (2,6)DEL 3:2)

--- src/tests/shared.js
+++ src/tests/shared.js
@@ -3,8 +3,7 @@ const config = require( '../config' );
 
 const gHeaders = {
     'Accept' : 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization' : 'alice',
+    'Content-Type' : 'application/json',
 };
 
 function apiSend( path, sendOptions ) {
@@ -13,10 +12,10 @@ function apiSend( path, sendOptions ) {
 
     debug && console.log( sFunc + 'path', path );
 
-    const newPath = config.apiServerPath + path
+    const newPath = config.apiServerPath + path;
 
-    if ( typeof( sendOptions.headers ) === 'undefined' )
-        sendOptions = { headers: sendOptions }
+    if ( typeof ( sendOptions.headers ) === 'undefined' )
+        sendOptions = { headers : sendOptions };
 
     return new Promise( ( respond /* reject*/ ) => {
         fetch( newPath, sendOptions )
@@ -27,5 +26,41 @@ function apiSend( path, sendOptions ) {
     } );
 }
 
-module.exports = { apiSend, gHeaders };
+function breakMutation( inMutation ) {
+    const sFunc = 'breakMutation()-->';
+    const debug = false;
+
+    // alice (2,6)INS 3:' big'
+    // bob (1,1)DEL 3:2
+
+    let length;
+    let text = '';
+    let x = inMutation.indexOf( ' ' );
+    let author = inMutation.substring( 0, x ).trim();
+    let y = inMutation.indexOf( ')' );
+    let origin = inMutation.substring( x + 1, y + 1 );
+    let type = inMutation.substring( y + 1, y + 4 );
+    let z = inMutation.indexOf( ':' );
+    let index = inMutation.substring( y + 6, z - 1 );
+    if ( type === 'DEL' ) {
+        length = inMutation.substring( z + 1 );
+    }
+    else {
+        text = inMutation.substring( z + 2 ).splice( -1, '', 1 );
+        length = text.length;
+    }
+
+    debug && console.log( sFunc + `returning: inMutation[${inMutation}] => author[${author}] origin[${origin}] type[${type}] index[${index}] length[${length}] text[${text}]` );
+
+    return ( { author, origin, type, index, length, text } );
+}
+
+String.prototype.splice = function( offset, text, removeCount = 0 ) {
+    let calculatedOffset = offset < 0 ? this.length + offset : offset;
+    return this.substring( 0, calculatedOffset ) +
+        text + this.substring( calculatedOffset + removeCount );
+};
+
+module.exports = { apiSend, gHeaders, breakMutation };
+
 
