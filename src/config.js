fb0aab3c372272fb2f15423d87a92abfe75f394f
Mike Anderson <mikea@candicontrols.com>
2021-06-14 14:15:32 -0700

    feat(config):
    * Adjusted for the prod and dev environments.

--- src/config.js
+++ src/config.js
@@ -1,13 +1,34 @@
+const fs = require( 'fs' );
+
+let apiHost;
+if ( fs.existsSync( process.cwd() + '/temp.log' ) ) {
+    apiHost = '127.0.0.1/';
+    exports.DB_SERVER = 'localhost';
+    exports.DB_PORT = 3389;
+    exports.DB_USERNAME = 'user';
+    exports.DB_PASSWORD = `Password01!`;
+    exports.DB_DATABASE = 'collab_editing_poc_server_db';
+
+}
+else {
+    apiHost = 'floating-hamlet-81896.herokuapp.com/';
+    exports.DB_SERVER = 'u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    exports.DB_PORT = 3306;
+    exports.DB_USERNAME = 'w9tvz0v7owbqkdgd';
+    exports.DB_PASSWORD = 'txgukn5y06hd1k0p';
+    exports.DB_DATABASE = 'n9cmqi0btiolj9xe';
+
+}
+
 exports.webPort = process.env.UI_PORT || 3000;
-exports.webURL = process.env.UI_PATH || `http://127.0.0.1:${exports.webPort}`;
+exports.webURL = process.env.UI_PATH || `http://${apiHost}:${exports.webPort}`;
 
 exports.msgServerPort = process.env.PORT || exports.webPort + 1;
-exports.msgServerPath = process.env.ORIGIN || `ws:127.0.0.1:${exports.msgServerPort}`;
+exports.msgServerPath = process.env.ORIGIN || `ws:${apiHost}:${exports.msgServerPort}`;
 
 exports.apiServerPort = process.env.PORT || exports.webPort + 2;
-exports.apiServerPath = process.env.ORIGIN || `http://127.0.0.1:${exports.apiServerPort}`;
-exports.apiServerPathRaw = process.env.ORIGIN || `http://127.0.0.1`;
-
+exports.apiServerPath = process.env.ORIGIN || `http://${apiHost}:${exports.apiServerPort}`;
+exports.apiServerPathRaw = process.env.ORIGIN || `http://${apiHost}`;
 
 exports.githubURL = 'https://github.com/svenmarley/collab-editing-poc';
 exports.authorName = 'Mike Anderson';
